0f13a62648cf0033c26497c3b4092d3ac1cac5f5
Nathan Simon <nsimon06@uw.edu>
2026-02-07 12:38:02 -0800

    first commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Nathanael Simon on 2/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
